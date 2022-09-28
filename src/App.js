e8b772173f11acd649c05b16d46cd464564997c8
StephanieRuthJones <stephanieruthjones@gmail.com>
2022-09-28 14:10:16 -0600

    Add demo code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [name, setName] = useState("");
+  const [effectMessage1, setEffectMessage1] = useState("");
+  const [effectMessage2, setEffectMessage2] = useState("");
+  const [effectMessage3, setEffectMessage3] = useState("");
+
+  // ************* NO DEPENDENCIES ************* //
+  useEffect(() => {
+    setEffectMessage1(
+      `Effect Hook, NO dependencies: You clicked ${count} times.`
+    );
+    console.log(
+      `Effect Hook, NO dependencies- run after every rendering (when count or name states update).`
+    );
+  });
+
+  // ************* EMPTY ARRAY DEPENDENCY ************* //
+  // useEffect(() => {
+  //   setEffectMessage2(
+  //     `Effect Hook, [] EMPTY ARRAY DEPENDENCY: You clicked ${count} times`
+  //   );
+  //   console.log(
+  //     `Effect Hook, [] EMPTY ARRAY DEPENDENCY - run only once when the component is mounted.`
+  //   );
+  // }, []);
+
+  // ************* COUNT DEPENDENCY ************* //
+  // useEffect(() => {
+  //   console.log(
+  //     `Effect Hook, [count] DEPENDENCY - runs only when count changes.`
+  //   );
+  //   setEffectMessage3(
+  //     `Effect Hook, [count] DEPENDENCY: You clicked ${count} times.`
+  //   );
+  // }, [count]);
+
+  const updateName = (e) => {
+    setName(e.target.value);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h1>Count: {count}</h1>
+        <p>{effectMessage1}</p>
+        <p>{effectMessage2}</p>
+        <p>{effectMessage3}</p>
+
+        <button onClick={() => setCount(count + 1)}>Increment</button>
+        <button onClick={() => setCount(count - 1)}>Decrement</button>
+        <button onClick={() => setCount(0)}>Reset</button>
+      </div>
+      <div>
+        <h1>Name: {name}</h1>
+        <input
+          id="name-input"
+          type="text"
+          placeholder="Name"
+          onChange={updateName}
+        />
+      </div>
     </div>
   );
 }
